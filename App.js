a5d3da3b958354818f76bcff185843486b7bd987
Matias Perez Hughes <hughesmatias@gmail.com>
2020-04-25 19:08:52 -0300

    Refactor to Camp component and integrate initial ui. Header with tabs

--- App.js
+++ App.js
@@ -1,235 +1,96 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Picker } from 'react-native';
-import Player from './components/Player';
+import React, { useState } from "react";
+import {
+  Container,
+  Header,
+  Title,
+  Tabs,
+  Tab,
+  Button,
+  Left,
+  Right,
+  Body,
+  Icon,
+} from "native-base";
+import { StyleSheet, Text, View, Picker } from "react-native";
+import Camp from "./components/Camp";
 
-export default function App() {
-  const options = [
-    [1, 4, 4, 2],
-    [1, 3, 4, 3],
-    [1, 5, 3, 2],
-    [1, 4, 3, 3],
-    [1, 3, 5, 2],
-    "custom"
-  ];
+import { AppLoading } from 'expo';
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
 
-  const [ optionSelected, setOption ] = useState(options[0].toString());
+const startTeam1 = {
+  color: "blue",
+  players: [1, 3, 2, 4, 19, 33, 5, 10, 16, 11, 21]
+};
 
-  const team1 = {
-    gk: [
-      {
-        number: 	1,
-        fullName:	"Petr Čech",
-      },{
-        number: 	40,
-        fullName:	"Ross Turnbull",
-      },{
-        number: 	22,
-        fullName:	"Hilário",
-      },
-    ],
-    defender: [
-    {
-      number: 	33,
-      fullName:	"Alex",
-    },
-    {
-      number: 	34,
-      fullName:	"Ryan Bertrand",
-    },
-    {
-      number: 	17,
-      fullName:	"Bosingwa",
-    },
-    {
-      number: 	3,
-      fullName:	"Ashley Cole",
-    },
-    {
-      number: 	4,
-      fullName:	"David Luiz",
-    },
-    {
-      number: 	2,
-      fullName:	"Branislav Ivanović",
-    },
-    {
-      number: 	19,
-      fullName:	"Paulo Ferreira",
-    },
-    {
-      number: 	26,
-      fullName:	"John Terry",
-    },
-  ],
-  md: [
-    {
-      number: 	10,
-      fullName:	"Yossi Benayoun",
-    },{
-      number: 	62,
-      fullName:	"Nathaniel Chalobah",
-    },{
-      number: 	47,
-      fullName:	"Billy Clifford",
-    },{
-      number: 	5,
-      fullName:	"Michael Essien",
-    },{
-      number: 	8,
-      fullName:	"Frank Lampard",
-    },{
-      number: 	15,
-      fullName:	"Florent Malouda",
-    },{
-      number: 	46,
-      fullName:	"Josh McEachran",
-    },{
-      number: 	33,
-      fullName:	"John Mikel",
-    },{
-      number: 	12,
-      fullName:	"Florent Malouda",
-    },{
-      number: 	16,
-      fullName:	"Ramires",
-    },{
-      number: 	7,
-      fullName:	"Jacopo Sala",
-    },{
-      number: 	55,
-      fullName:	"Gökhan Töre",
-    },{
-      number: 	18,
-      fullName:	"Yuriy Zhirkov",
-    },
-  ],
-  forward:[
-    {
-      number: 	39,
-      fullName:	"Nicolas Anelka",
-    },{
-      number: 	11,
-      fullName:	"Didier Drogba",
-    },{
-      number: 	9,
-      fullName:	"Fernando Torres",
-    },{
-      number: 	21,
-      fullName:	"Salomon Kalou",
-    },
-  ]
-  };
+const startTeam2 = {
+  color: "red",
+  players: [40, 3, 2, 4, 19, 33, 5, 10, 16, 11, 21]
+};
 
-  const startTeam1 = {
-    color: "blue",
-    players: [1, 3, 2, 4, 19, 33, 5, 10, 16, 11, 21]
-  };
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
 
-  const startTeam2 = {
-    color: "red",
-    players: [40, 3, 2, 4, 19, 33, 5, 10, 16, 11, 21]
-  };
-  // {
-  //   gk: [1],
-  //   defender: [3, 2, 4, 19],
-  //   md: [33, 5, 10, 16],
-  //   forward: [11, 21]
-  // };
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
 
-  const getFormation = (team, indice) => {
-    if (indice == 0) {
-      return team.players.splice(0, 1)
-    } else if (indice == 1) {
-      return team.players.splice(0, parseInt(optionSelected.split(",")[indice]))
-    } else if (indice == 2) {
-      return team.players.splice(0, parseInt(optionSelected.split(",")[indice]))
-    } else {
-      return team.players;
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
     }
-  }
 
-  const isPlaing = player => 
-    startTeam1.players.includes(st => st == player) && startTeam2.players.includes(st => st == player);
-
-  const generateTeam = (currentTeam) => {
-    const lines = optionSelected.split(",");
-    // currentTeam.players.push()
-  }
-
-  const renderField = team => {
     return (
-      <View style={{ flex: 1 }}>
-        {getFormation(team, 0).map((number, key) => <Player position={{
-          x: 50,
-          y: 20
-        }} number={number} key={key} />)}
-        {getFormation(team, 1).map((number, key) => <Player position={{
-          x: 100,
-          y: 200
-        }} number={number} key={key} />)}
-        {getFormation(team, 2).map((number, key) => <Player position={{
-          x: 100,
-          y: 400
-        }} number={number} key={key} />)}
-        {getFormation(team, 3).map((number, key) => <Player position={{
-          x: 100,
-          y: 600
-        }} number={number} key={key} />)}
-      </View>
+      <Container>
+        <Header hasTabs>
+          <Left>
+            <Button transparent>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Team Manager</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Tabs>
+          <Tab heading="Team 1">
+            <Camp team={startTeam1} />
+          </Tab>
+
+          <Tab heading="Team 2">
+            <Camp team={startTeam2} />
+          </Tab>
+        </Tabs>
+      </Container>
     );
   }
-
-  const renderDinamic = team => {
-    <View style={{
-      flex: 1,
-    }}>
-      <View style={styles.row}>
-        {team.players.map((number, key) => {
-          return <Player position={{
-            x: 100,
-            y: 100
-          }} number={number} key={key} />
-        })}
-      </View>
-    </View>
-  }
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.campo}>
-        <View style={styles.information}>
-          <Text>Informacion seteada</Text>
-          <Picker
-            selectedValue={optionSelected}
-            style={{ height: 50, width: 150 }}
-            onValueChange={(itemValue, itemIndex) => setOption(itemValue)}
-          >
-            {options.map(op => <Picker.Item label={op.toString()} value={op.toString()} />)}
-          </Picker>
-        </View>
-        {optionSelected == "custom" ? renderDinamic(startTeam1) : renderField(startTeam1)}
-        <View style={styles.information}>
-          <Text>menu inferior</Text>
-        </View>
-      </View>
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'transparent',
+    backgroundColor: "transparent",
     paddingTop: 34,
   },
   campo: {
     flex: 1,
-    backgroundColor: 'green',
+    backgroundColor: "green",
   },
   information: {
     backgroundColor: "pink",
     height: 70,
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   row: {
     borderTopWidth: 2,
@@ -237,5 +98,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     flexDirection: "row",
     alignItems: "center",
-  }
+  },
 });
---
+++ components/Camp.js
@@ -0,0 +1,239 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Picker } from 'react-native';
+import Player from './Player';
+
+const camp = (props) => {
+  const options = [
+    [1, 4, 4, 2],
+    [1, 3, 4, 3],
+    [1, 5, 3, 2],
+    [1, 4, 3, 3],
+    [1, 3, 5, 2],
+    "custom"
+  ];
+
+  const [ optionSelected, setOption ] = useState(options[0].toString());
+
+  const team1 = {
+    gk: [
+      {
+        number: 	1,
+        fullName:	"Petr Čech",
+      },{
+        number: 	40,
+        fullName:	"Ross Turnbull",
+      },{
+        number: 	22,
+        fullName:	"Hilário",
+      },
+    ],
+    defender: [
+    {
+      number: 	33,
+      fullName:	"Alex",
+    },
+    {
+      number: 	34,
+      fullName:	"Ryan Bertrand",
+    },
+    {
+      number: 	17,
+      fullName:	"Bosingwa",
+    },
+    {
+      number: 	3,
+      fullName:	"Ashley Cole",
+    },
+    {
+      number: 	4,
+      fullName:	"David Luiz",
+    },
+    {
+      number: 	2,
+      fullName:	"Branislav Ivanović",
+    },
+    {
+      number: 	19,
+      fullName:	"Paulo Ferreira",
+    },
+    {
+      number: 	26,
+      fullName:	"John Terry",
+    },
+  ],
+  md: [
+    {
+      number: 	10,
+      fullName:	"Yossi Benayoun",
+    },{
+      number: 	62,
+      fullName:	"Nathaniel Chalobah",
+    },{
+      number: 	47,
+      fullName:	"Billy Clifford",
+    },{
+      number: 	5,
+      fullName:	"Michael Essien",
+    },{
+      number: 	8,
+      fullName:	"Frank Lampard",
+    },{
+      number: 	15,
+      fullName:	"Florent Malouda",
+    },{
+      number: 	46,
+      fullName:	"Josh McEachran",
+    },{
+      number: 	33,
+      fullName:	"John Mikel",
+    },{
+      number: 	12,
+      fullName:	"Florent Malouda",
+    },{
+      number: 	16,
+      fullName:	"Ramires",
+    },{
+      number: 	7,
+      fullName:	"Jacopo Sala",
+    },{
+      number: 	55,
+      fullName:	"Gökhan Töre",
+    },{
+      number: 	18,
+      fullName:	"Yuriy Zhirkov",
+    },
+  ],
+  forward:[
+    {
+      number: 	39,
+      fullName:	"Nicolas Anelka",
+    },{
+      number: 	11,
+      fullName:	"Didier Drogba",
+    },{
+      number: 	9,
+      fullName:	"Fernando Torres",
+    },{
+      number: 	21,
+      fullName:	"Salomon Kalou",
+    },
+  ]
+  };
+
+  const startTeam1 = {
+    color: "blue",
+    players: [1, 3, 2, 4, 19, 33, 5, 10, 16, 11, 21]
+  };
+
+  const startTeam2 = {
+    color: "red",
+    players: [40, 3, 2, 4, 19, 33, 5, 10, 16, 11, 21]
+  };
+  // {
+  //   gk: [1],
+  //   defender: [3, 2, 4, 19],
+  //   md: [33, 5, 10, 16],
+  //   forward: [11, 21]
+  // };
+
+  const getFormation = (team, indice) => {
+    if (indice == 0) {
+      return team.players.splice(0, 1)
+    } else if (indice == 1) {
+      return team.players.splice(0, parseInt(optionSelected.split(",")[indice]))
+    } else if (indice == 2) {
+      return team.players.splice(0, parseInt(optionSelected.split(",")[indice]))
+    } else {
+      return team.players;
+    }
+  }
+
+  const isPlaing = player => 
+    startTeam1.players.includes(st => st == player) && startTeam2.players.includes(st => st == player);
+
+  const generateTeam = (currentTeam) => {
+    const lines = optionSelected.split(",");
+    // currentTeam.players.push()
+  }
+
+  const renderField = team => {
+    return (
+      <View style={{ flex: 1 }}>
+        {getFormation(team, 0).map((number, key) => <Player position={{
+          x: 50,
+          y: 20
+        }} number={number} key={key} />)}
+        {getFormation(team, 1).map((number, key) => <Player position={{
+          x: 100,
+          y: 200
+        }} number={number} key={key} />)}
+        {getFormation(team, 2).map((number, key) => <Player position={{
+          x: 100,
+          y: 400
+        }} number={number} key={key} />)}
+        {getFormation(team, 3).map((number, key) => <Player position={{
+          x: 100,
+          y: 600
+        }} number={number} key={key} />)}
+      </View>
+    );
+  }
+
+  const renderDinamic = team => {
+    <View style={{
+      flex: 1,
+    }}>
+      <View style={styles.row}>
+        {team.players.map((number, key) => {
+          return <Player position={{
+            x: 100,
+            y: 100
+          }} number={number} key={key} />
+        })}
+      </View>
+    </View>
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.campo}>
+        <View style={styles.information}>
+          <Picker
+            selectedValue={optionSelected}
+            style={{ height: 50, width: 150 }}
+            onValueChange={(itemValue, itemIndex) => setOption(itemValue)}
+          >
+            {options.map(op => <Picker.Item label={op.toString()} value={op.toString()} />)}
+          </Picker>
+        </View>
+        {optionSelected == "custom" ? renderDinamic(props.team) : renderField(props.team)}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'transparent',
+  },
+  campo: {
+    flex: 1,
+    backgroundColor: 'green',
+  },
+  information: {
+    backgroundColor: "pink",
+    height: 70,
+    justifyContent: 'center',
+  },
+  row: {
+    borderTopWidth: 2,
+    borderTopColor: "grey",
+    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+  }
+});
+
+export default camp;
